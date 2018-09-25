49fb4370136fede968027bc76f75b80e0b6969eb
Falak Singhal <Falak.Singhal@copart.com>
2018-09-25 12:58:52 -0500

    Reverse a LinkedList

---
+++ src/leetorg4g/ReverseLL.java
@@ -0,0 +1,27 @@
+package leetorg4g;
+
+import basicds.Node;
+
+public class ReverseLL {
+
+    public Node reverseList(Node head) {
+        if(head==null) return null;
+
+        Node cur=head;
+        Node prev=null;
+        Node next=null;
+
+        while(cur !=null){
+            next=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
